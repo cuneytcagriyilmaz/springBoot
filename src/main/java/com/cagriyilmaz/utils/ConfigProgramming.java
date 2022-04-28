27a6a9a22826ac59b492e9e544ceba4cda46a670
cuneytcagriyilmaz <88775443+cuneytcagriyilmaz@users.noreply.github.com>
2022-04-28 20:15:01 +0300

    Configurations

---
+++ src/main/java/com/cagriyilmaz/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.cagriyilmaz.utils;
+
+public class ConfigProgramming {
+    //Devtools ==> LiveReolad
+    //file ==> settings ==> compiler ==> Build Project Automaticly (CTRL + ALT + S)
+    // file ==> Advanced Settings ==> Allow Auto make to start even(OK)...
+}
---
+++ src/main/java/com/cagriyilmaz/utils/IntellijShortcut.java
@@ -0,0 +1,9 @@
+package com.cagriyilmaz.utils;
+
+public class IntellijShortcut {
+//    Setting ==> ctrl + alt + s
+//    Alt + Ins ==> constructor, toString, getter, setter
+//    ctrl +/ => single comment
+//    ctrl+shift+/ ==> multiple comment
+//
+}
