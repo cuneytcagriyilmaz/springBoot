1804b3524c870a53944df6e3cf5c69d42134f7a8
cuneytcagriyilmaz <88775443+cuneytcagriyilmaz@users.noreply.github.com>
2022-04-30 18:29:29 +0300

    Model Object ve Path

--- src/main/java/com/cagriyilmaz/controller/ThymeleafController.java
+++ src/main/java/com/cagriyilmaz/controller/ThymeleafController.java
@@ -1,14 +1,21 @@
 package com.cagriyilmaz.controller;
 
 
+import com.cagriyilmaz.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ThymeleafController {
 
+    // http://localhost:8001
+    // hem index yazınca hemde hiçbirşe yazmayınca index sayfasına gider. Opsiyonel yapı
     @GetMapping({"/", "index"})
     public String index() {
         return "index";
@@ -45,6 +52,52 @@ public class ThymeleafController {
         model.addAttribute("key_model1", "Ben Modelden geldim-1");
         model.addAttribute("key_model2", "Ben Modelden geldim-4");
         return "thymeleaf4";
+    }
+
+    /////////////////////////////////
+    // Modelde Object Göndermek
+    //http://localhost:8001/thymeleaf5
+    @GetMapping("/thymeleaf5")
+    public String getThymeleaf5ModelObject(Model model) {
+        model.addAttribute("key_model1", "text");
+        ProductDto productDto =
+                ProductDto
+                        .builder()
+                        .productId(0L)
+                        .productName("Ürün Adı")
+                        .productPrice(2500)
+                        .build();
+        model.addAttribute("key_model2",productDto);
+        return "thymeleaf5";
+    }
+
+    // Modelde Objkect List Göndermek
+    //http://localhost:8001/thymeleaf6
+    @GetMapping("/thymeleaf6")
+    public String getThymeleaf6ModelObjectList(Model model) {
+        model.addAttribute("key_model1", "text");
+        List<ProductDto> Listem = new ArrayList<>();
+        Listem.add(ProductDto.builder().productId(1L).productName("Ürün Adı1").productPrice(1500).build());
+        Listem.add(ProductDto.builder().productId(2L).productName("Ürün Adı2").productPrice(2500).build());
+        Listem.add(ProductDto.builder().productId(3L).productName("Ürün Adı3").productPrice(3500).build());
+        Listem.add(ProductDto.builder().productId(4L).productName("Ürün Adı4").productPrice(4500).build());
+
+
+        model.addAttribute("product_liste",Listem);
+        return "thymeleaf6";
+    }
+
+    //http://localhost:8001/thymeleaf7/4
+    @GetMapping({"/thymeleaf7","/thymeleaf7/{id}"})
+    public String getThymeleaf7ModelObjectList(Model model, @PathVariable(name = "id", required = false)Long id) {
+        if (id!=null){
+            model.addAttribute("key_model1", "id" + id);
+
+        }else {
+            model.addAttribute("key_model1", "id Bulunamadı" + id);
+
+        }
+        return "thymeleaf7";
     }
 
 
