82f7ae534880962d6f3fedea5be4dfc771206395
cuneytcagriyilmaz <88775443+cuneytcagriyilmaz@users.noreply.github.com>
2022-05-01 16:59:09 +0300

    EmploteeDto

---
+++ src/main/java/com/cagriyilmaz/dto/EmploteeDto.java
@@ -0,0 +1,21 @@
+package com.cagriyilmaz.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+//DTO DATA TRANSFER OBJECT
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+public class EmploteeDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+}
