d8ea98ba7bbabc81f4ab89072816d0692bdc6f0b
cuneytcagriyilmaz <88775443+cuneytcagriyilmaz@users.noreply.github.com>
2022-05-01 17:30:05 +0300

    @Repository (Jpa Hibernate)
    extends JpaRepository
    extends CrudRepository

---
+++ src/main/java/com/cagriyilmaz/repository/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.cagriyilmaz.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+//JPA yapısı Crud'dan daha geniştir.
+public interface EmployeeRepository extends JpaRepository {
+    //Kodlama içerisindeki JPA Hibernate işlemlerini kullanacak yapılardır
+}
+
+ */
+
+@Repository
+public interface EmployeeRepository extends CrudRepository {
+    //Kodlama içerisindeki JPA Hibernate işlemlerini kullanacak yapılardır
+}
